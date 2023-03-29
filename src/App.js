3d6ee4649f9971fe90229fa096e1b81f7bce5f2d
Mikhail Burmistrov <misha.burmistrov@gmail.com>
2023-03-29 17:38:27 +0200

    Implement basic what's new html helper prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
+import Output from "./Output";
 
 function App() {
+  const [post, setPost] = useState({
+    title: "User roles",
+    date: "March 10, 2023",
+    platform: "Cloud",
+    quarter: "2023 q1",
+    imageName: "61-use-roles.png"
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="fields">
+        <label class="field">
+          Title
+          <input type="text" value={post.title} onInput={e => setPost({...post, title: e.target.value }) } />
+        </label>
+        <label class="field">
+          Date
+          <input type="text" value={post.date} onInput={e => setPost({...post, date: e.target.value }) } />
+        </label>
+        <label class="field">
+          Plaform
+          <input type="text" value={post.platform} onInput={e => setPost({...post, platform: e.target.value }) } />
+        </label>
+        <label className="field">
+          Quarter
+          <input type="text" value={post.quarter} onInput={e => setPost({...post, quarter: e.target.value})}/>
+        </label>
+        <label className="field">
+          Image name
+          <input type="text" value={post.imageName} onInput={e => setPost({...post, imageName: e.target.value})}/>
+        </label>
+      </div>
+      <Output post={post} />
     </div>
   );
 }
---
+++ src/Output.js
@@ -0,0 +1,67 @@
+import SyntaxHighlighter from 'react-syntax-highlighter';
+
+const getOutputForPost = (post) => {
+  const attrFormatTitle = post.title.toLowerCase().split(" ").join("-");
+  const attrFormatPlatform = post.platform.toLowerCase();
+  const attrFormatQuarter = post.quarter.toLowerCase().split(" ").join("-");
+
+  return `
+<section className="wt-section wt-section_bg_dark wt-section_theme_dark package-management ${attrFormatPlatform}">
+  <div className="wt-container">
+    <div className="f-section">
+      <div className="f-title"
+         data-title="scope ${post.quarter} ${post.title}"
+         data-filter="${attrFormatPlatform}"
+         title="${post.title}">
+        <div
+          className="wt-text-3 wt-text-1_hardness_hard wt-text-3_theme_dark wt-row wt-row_size_xs f-title__data">
+          <div className="wt-col-inline">${post.date}</div>
+          <div className="wt-col-inline copy-link"
+             data-href="#scope-${attrFormatQuarter}-${attrFormatTitle}"></div>
+          <div className="wt-col-inline"><span className="wn-tag">Package management</span></div>
+        </div>
+        <div className="wt-row wt-row_size_m wt-row_justify_between">
+          <h2 className="wt-h2 wt-h2_theme_dark wt-offset-top-8 wt-col-8 wt-col-sm-12">
+            ${post.title}
+          </h2>
+          <div className="wt-col-4 wt-col-sm-12 wt-offset-top-sm-16 version-tags">
+            <span className="f-badge f-badge_text-normal ${attrFormatPlatform}">${post.platform}</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="f-block f-block_1-of-1 f-block_img-right">
+        <a
+          className="js-popup popup _with-icon _show-img f-img f-img_1-of-2"
+          href-english="/space/whatsnew/img/${attrFormatQuarter}/${post.imageName}"
+        >
+          <img
+            className="f-img"
+            src="/space/whatsnew/img/${attrFormatQuarter}/${post.imageName}"
+            alt="${post.title}"
+            width="600"
+            loading="eager"
+          />
+        </a>
+        <div className="f-description">
+          <div className="f-text f-text_theme_dark">
+            <p>
+              Content will be here
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>`;
+}
+function Output({ post }) {
+  const outputString = getOutputForPost(post);
+  return (
+  <SyntaxHighlighter customStyle={{'font-size': '12px'}}>
+    {outputString}
+  </SyntaxHighlighter>
+  );
+}
+
+export default Output;
